c8f293f397b5c0e59b23c87d520f9746c7c7d1f6
Amaan <amaanshaikh.gg@gmail.com>
2024-07-26 00:41:29 +0530

    add files

--- client/src/components/shared/Banner.js
+++ client/src/components/shared/Banner.js
@@ -35,7 +35,27 @@ const Banner = ({ query, handleInput }) => {
               }}
             />
           </div>
-
+          {/* <div className="d-flex md-rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within-ring-2 focus-within-ring-inset focus-within-ring-indigo-600 col-md-4 col-12">
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Location"
+              style={{ paddingLeft: "2rem" }}
+              className="form-control d-block flex-1 border-2 bg-transparent text-gray-900 placeholder-text-gray-400 focus-right-0 form-control-sm form-control-sm-leading-6"
+              onChange={handleCompany}
+              value={query1}
+            />
+            <FontAwesomeIcon
+              icon={faLocationDot}
+              style={{
+                position: "absolute",
+                marginLeft: "0.8rem",
+                marginTop: "0.8rem",
+                color: "#9CA3AF",
+              }}
+            />
+          </div> */}
           <button
             type="submit"
             className="bg-primary border-0 py-2 px-5 border-1 text-white md-rounded-s-none rounded"

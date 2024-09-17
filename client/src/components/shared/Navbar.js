23f0472c14a76dd55c056ec047165bd4370c3089
Amaan <amaanshaikh.gg@gmail.com>
2024-09-17 18:11:33 +0530

    add files

--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
         .then((res) => res.json())
         .then((data) => {
           if (data.success) {
-            setUser(data.data);
+            setUser(data.data.userRole);
             //console.log(data);
           } else {
             localStorage.clear();

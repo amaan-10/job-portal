c725028a39e3fe91aa179c31e6def2bc6c4eb58f
Amaan <amaanshaikh.gg@gmail.com>
2025-01-07 13:46:47 +0530

    demo login added

---
+++ client/src/components/shared/DemoLogin.js
@@ -0,0 +1,141 @@
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { hideLoading, showLoading } from "../../redux/features/alertSlice";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import { BASE_URL } from "../../url";
+
+export default function DemoLogin() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [demo, setDemo] = useState(false);
+
+  //check if user is logged in or not
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setLoggedIn(!!token);
+  }, []);
+  const navigate = useNavigate();
+  const dispach = useDispatch();
+
+  const handleDemoLogin = async (role) => {
+    // Dummy credentials
+    const email =
+      role === "job-seeker"
+        ? "demo-seeker@empolymee.com"
+        : "demo-recruiter@empolymee.com";
+    const password = role === "job-seeker" ? "seeker123" : "recruiter123";
+
+    try {
+      dispach(showLoading());
+      const { data } = await axios.post(`${BASE_URL}/api/v1/auth/login`, {
+        email,
+        password,
+      });
+      if (data.success) {
+        dispach(hideLoading());
+        localStorage.setItem("token", data.token);
+        toast.success("Login Successfull");
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      console.error("Demo login error:", error);
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
+
+  return (
+    <>
+      {!isLoggedIn ? (
+        <div className="position-fixed bottom-0 end-0 mb-3 me-3 rounded-circle d-flex flex-column align-items-end justify-content-center bg-transparent border-0 ">
+          <div
+            className="popover-container bg-light shadow p-3 mb-3 rounded"
+            style={{
+              zIndex: 1050,
+              width: "275px",
+            }}
+          >
+            {!demo ? (
+              <>
+                <h6 className="popover-title">Curious about the platform?</h6>
+                <p className="popover-content mb-0">
+                  Try it now with demo credentials
+                </p>
+                <div
+                  className="popover-arrow"
+                  style={{
+                    zIndex: 1050,
+                    position: "absolute",
+                    top: "85px",
+                    left: "92%",
+                    transform: "translateX(-50%)",
+                    width: 0,
+                    height: 0,
+                    borderLeft: "10px solid transparent",
+                    borderRight: "10px solid transparent",
+                    borderTop: "10px solid #f8f9fa",
+                  }}
+                />
+              </>
+            ) : (
+              <>
+                <h6 className="popover-title">Try Demo:</h6>
+                <button
+                  className="btn btn-primary mb-2 demo-button seeker"
+                  onClick={() => handleDemoLogin("job-seeker")}
+                >
+                  as a Job Seeker
+                </button>
+                <button
+                  className="btn btn-primary demo-button recruiter"
+                  onClick={() => handleDemoLogin("recruiter")}
+                >
+                  as a Recruiter
+                </button>
+                <div
+                  className="popover-arrow"
+                  style={{
+                    zIndex: 1050,
+                    position: "absolute",
+                    top: "142px",
+                    left: "92%",
+                    transform: "translateX(-50%)",
+                    width: 0,
+                    height: 0,
+                    borderLeft: "10px solid transparent",
+                    borderRight: "10px solid transparent",
+                    borderTop: "10px solid #f8f9fa",
+                  }}
+                />
+              </>
+            )}
+          </div>
+
+          <button
+            className="border-0 bg-transparent"
+            onClick={() => setDemo(true)}
+            style={{
+              transform: isHovered ? "scale(1.1)" : "scale(1)",
+              transition: "transform 0.2s",
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            aria-label="Toggle Theme"
+          >
+            <FontAwesomeIcon
+              icon={faCircleExclamation}
+              shake
+              style={{ color: "#0d6efd", fontSize: "2.5rem" }}
+            />
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}

023394bda920b3824d640174e0b0f52cf00c8dcd
Amaan <amaanshaikh.gg@gmail.com>
2025-01-08 16:31:09 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import ApplicantDetails from "./pages/ApplicantDetails";
 import Footer from "./components/shared/Footer";
 import UserRoute from "./components/routes/UserRoute";
 import DemoLogin from "./components/shared/DemoLogin";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -188,6 +189,7 @@ function App() {
           }
         />
       </Routes>
+      <Analytics />
     </>
   );
 }

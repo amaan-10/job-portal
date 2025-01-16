6b8778e6be86c4cc4761c5f8320418e4a3325fda
Amaan <amaanshaikh.gg@gmail.com>
2025-01-16 13:17:43 +0530

    uvicorn run added

--- ai-service/app.py
+++ ai-service/app.py
@@ -49,3 +49,7 @@ async def recommend_jobs(request: JobRecommendationRequest):
     
     recommendations = [jobs[i[0]] for i in ranked_jobs if i[1] > 0.2]
     return recommendations
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)

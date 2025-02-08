70824c9f690e43523af9a48b39c966eb3e048346
Amaan <amaanshaikh.gg@gmail.com>
2025-02-08 23:33:30 +0530

    fix: layout

--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -296,28 +296,37 @@ const Navbar = () => {
           </div>
         </nav>
         <div
-          className={`mt-2 me-2 px-4 py-3 position-absolute end-0 z-1 bg-white border shadow-sm rounded d-md-none rounded-sm ${
+          className={`position-absolute end-0 d-md-none ${
             menuOpen ? "" : "d-none"
           }`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 320 512"
-            className="position-fixed"
+          <div
             style={{
-              top: "54px",
-              right: "10px",
-              width: "32px",
-              height: "32px",
+              textAlign: "right",
+              height: "14px",
             }}
-            fill="white"
-            stroke={"#dee2e6"}
-            strokeWidth={14}
           >
-            <path d="M182.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 200"
+              style={{
+                width: "32px",
+                height: "14px",
+                marginRight: "10px",
+                marginBottom: "10px",
+              }}
+              fill="white"
+              stroke={"#dee2e6"}
+              strokeWidth={14}
+            >
+              <path d="M182.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
+            </svg>
+          </div>
 
-          <ul className="p-0 m-0" style={{ listStyleType: "none" }}>
+          <ul
+            className="me-2 px-4 py-3 end-0 z-1 bg-white border shadow-sm rounded d-md-none rounded-sm"
+            style={{ listStyleType: "none" }}
+          >
             {navItems.map(({ path, title }) => (
               <li key={path}>
                 <NavLink
